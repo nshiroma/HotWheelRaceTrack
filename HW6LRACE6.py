4b6aca28ce0f271bd97c64fbd3a711b66d4a013a
nshiroma <nshiroma@funaiservice.com>
2020-09-10 21:45:57 -0400

    optimized file using dictionaries

---
+++ HW6LRACE6.py
@@ -0,0 +1,168 @@
+#!/usr/bin/python
+# you have to include the spidev module
+import spidev
+import time
+
+# put these functions and variables  at the
+# top of your python project or in another
+# file and import it
+A={}
+A[0]=0
+A[1]=1
+A[2]=2
+A[3]=3
+A[4]=4
+A[5]=5
+# A0 = 0
+# A1 = 1
+# A2 = 2
+# A3 = 3
+# A4 = 4
+# A5 = 5
+# A6 = 6
+# A7 = 7
+
+
+def adcInit():
+    connection = spidev.SpiDev()
+    connection.open(0, 0)
+    connection.max_speed_hz = 1350000
+    connection.mode = 0b00
+    return connection
+
+def adcRead(connection, channel):
+    assert 0 <= channel <= 7, 'ADC number has to be 0-7'
+    ret = connection.xfer2([0x01, (8 + channel) << 4, 0x00 ])
+    tmp = (ret[1] & 0x03) << 9
+    tmp |= (ret[2] & 0xFF)
+    return tmp
+
+def time_convert(sec):
+  mins = sec // 60
+  sec = sec % 60
+  hours = mins // 60
+  mins = mins % 60
+  print("Time Lapsed = {0}:{1}:{2}".format(int(hours),int(mins),sec))
+#-------------------------------------------------------
+
+# only call adcInit() once in you project close to
+# the top before calling adcRead()
+adc = adcInit()
+line1 = 0
+line2 = 0
+line3 = 0
+line4 = 0
+line5 = 0
+line6 = 0
+looptrue = True
+input("Press Enter to start")
+start_time = time.time()
+last_triger_time1 =start_time
+last_triger_time2 =start_time
+last_triger_time3 =start_time
+last_triger_time4 =start_time
+last_triger_time5 =start_time
+last_triger_time6 =start_time
+while looptrue:
+    # the first argument to adcRead() is the reurn from adcInit()
+    # the second argument is the analog pin to read A0-A7
+    data = adcRead(adc, A[0])
+    data1 = adcRead(adc, A[1])
+    data2 = adcRead(adc, A[2])
+    data3 = adcRead(adc, A[3])
+    data4 = adcRead(adc, A[4])
+    data5 = adcRead(adc, A[5])
+    if line1 == 0:
+        if data > 300:
+            end_time = time.time()
+            time_lapsed = end_time - start_time
+            time_lapsed_bitween_las_trigger = end_time - last_triger_time1
+            #            print(time_lapsed_bitween_las_trigger)
+            last_triger_time1 = end_time
+            #            time_convert(time_lapsed_bitween_las_trigger)
+            if time_lapsed_bitween_las_trigger < 0.005:
+                line1 = 1
+                print(data)
+                #               print(time_lapsed_bitween_las_trigger)
+                print("line1")
+                time_convert(time_lapsed)
+    if line1 == 0:
+        if data > 300:
+            end_time = time.time()
+            time_lapsed = end_time - start_time
+            time_lapsed_bitween_las_trigger = end_time -last_triger_time1
+#            print(time_lapsed_bitween_las_trigger)
+            last_triger_time1 = end_time
+#            time_convert(time_lapsed_bitween_las_trigger)
+            if time_lapsed_bitween_las_trigger < 0.005:
+                line1 = 1
+                print(data)
+#               print(time_lapsed_bitween_las_trigger)
+                print("line1")
+                time_convert(time_lapsed)
+    if line2 == 0:
+        if data1 > 300:
+            end_time = time.time()
+            time_lapsed = end_time - start_time
+            time_lapsed_bitween_las_trigger = end_time - last_triger_time2
+            last_triger_time2 = end_time
+            # time_convert(time_lapsed_bitween_las_trigger)
+            if time_lapsed_bitween_las_trigger < 0.005:
+                line2 = 1
+                print(data1)
+                print("line2")
+                time_convert(time_lapsed)
+    if line3 == 0:
+        if data2 > 300:
+            end_time = time.time()
+            time_lapsed = end_time - start_time
+            time_lapsed_bitween_las_trigger = end_time - last_triger_time3
+            last_triger_time3 = end_time
+                #            time_convert(time_lapsed_bitween_las_trigger)
+            if time_lapsed_bitween_las_trigger < 0.005:
+                line3 = 1
+                print(data2)
+                print("line3")
+                time_convert(time_lapsed)
+    if line4 == 0:
+        if data3 > 300:
+            end_time = time.time()
+            time_lapsed = end_time - start_time
+            time_lapsed_bitween_las_trigger = end_time - last_triger_time4
+            last_triger_time4 = end_time
+            #            time_convert(time_lapsed_bitween_las_trigger)
+            if time_lapsed_bitween_las_trigger < 0.005:
+                line4 = 1
+                print(data3)
+                print("line4")
+                time_convert(time_lapsed)
+    if line5 == 0:
+        if data4 > 300:
+            end_time = time.time()
+            time_lapsed = end_time - start_time
+            time_lapsed_bitween_las_trigger = end_time - last_triger_time5
+            last_triger_time5 = end_time
+            #            time_convert(time_lapsed_bitween_las_trigger)
+            if time_lapsed_bitween_las_trigger < 0.005:
+                line5 = 1
+                print(data4)
+                print("line5")
+                time_convert(time_lapsed)
+    if line6 == 0:
+        if data5 > 300:
+            end_time = time.time()
+            time_lapsed = end_time - start_time
+            time_lapsed_bitween_las_trigger = end_time - last_triger_time6
+            last_triger_time6 = end_time
+            # time_convert(time_lapsed_bitween_las_trigger)
+            if time_lapsed_bitween_las_trigger < 0.005:
+                line6 = 1
+                print(data5)
+                print("line6")
+                time_convert(time_lapsed)
+    race_time=time.time()
+    race_time_lasped = race_time-start_time
+    if race_time_lasped > 30:
+        looptrue = False
+        print("Race time out")
+
